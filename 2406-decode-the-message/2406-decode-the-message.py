dff70ed65fcc594379ef66bc098740a300456e9b
Satya Durga Paladugula <100195735+SatyaDurgaPaladugula@users.noreply.github.com>
2024-10-18 11:52:24 +0530

    Time: 3 ms (100%), Space: 16.6 MB (71.27%) - LeetHub

---
+++ 2406-decode-the-message/2406-decode-the-message.py
@@ -0,0 +1,15 @@
+class Solution:
+    def decodeMessage(self, key: str, message: str) -> str:
+        dici={}
+        temp=97
+        for i in key:
+            if i!=" " and i not in dici:
+                dici[i]=chr(temp)
+                temp+=1
+        ans=""
+        for i in message:
+            if i==" ":
+               ans+=" "
+            else:
+                ans+=dici[i]
+        return ans
